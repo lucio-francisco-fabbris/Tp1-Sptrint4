60c8348927bb5221b512e4afec3ddd0f5bbe03d4
Lucio Francisco <franfa0610@gmail.com>
2026-05-05 19:13:42 -0300

    Implement CRUD functionality for superheroes and enhance UI styling

--- ejs-layout-example/app.mjs
+++ ejs-layout-example/app.mjs
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/superheroes', (req, res) => {
-    res.render('superheroes', {title: 'Lista', navbar, superheroes});
+    res.render('superheroes', {title: 'Lista', navbar, superheroes, success: req.query.success });
 });
 
 app.get('/add', (req, res) => {
@@ -34,12 +34,42 @@ app.get('/add', (req, res) => {
 
 app.post('/add', (req, res) => {
     const { nombre } = req.body;
-    if (!nombre) {
+
+    console.log(superheroes);
+
+    if (!nombre || nombre.trim() === '') {
         return res.render('addSuperhero', {title: 'Agregar', navbar, error: 'El nombre es obligatorio'});
     }
     superheroes.push({ nombre });
+
+        res.redirect('/superheroes?success=1');
+});
+
+app.get('/delete/:index', (req, res) => {
+    const index = req.params.index;
+
+    superheroes.splice(index, 1);
+
     res.redirect('/superheroes');
 });
+
+app.get('/edit/:index', (req, res) => {
+    const index = req.params.index;
+
+    res.render('editSuperhero', {title: 'Editar', navbar, hero: superheroes[index], index});
+});
+
+app.post('/edit/:index', (req, res) => {
+    const index = req.params.index;
+    const { nombre } = req.body;
+
+    superheroes[index].nombre = nombre;
+
+    res.redirect('/superheroes');
+
+})
+
+
 
 
 app.listen(PORT, () => {
